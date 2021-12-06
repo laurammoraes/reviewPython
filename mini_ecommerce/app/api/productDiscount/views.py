721f9eaca6027fdecce46094131808666c5b469e
unknown <laura.moraes@faci.ly>
2021-12-06 14:37:19 -0300

    implemantacao do discount sem erro

--- mini_ecommerce/app/api/productDiscount/views.py
+++ mini_ecommerce/app/api/productDiscount/views.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, status
 from typing import List
 from fastapi import Depends
 from app.models.models import ProductDiscount
-from mini_ecommerce.app.models.models import Product
+from app.models.models import Product
 from .schemas import ProductDiscountSchema
-from .schemas import ShowProductDiscountSchema
+
 from sqlalchemy.orm import Session
 from app.db.db import get_db
 from app.repositories.product_discount_repository import ProductDiscountRepository
@@ -19,15 +19,15 @@ def create(productDiscount: ProductDiscountSchema, service: ProductDiscountServi
     service.create_discount(productDiscount)
    
 
-@router.get('/', response_model=List[ShowProductDiscountSchema])
-def index(repository: ProductDiscountRepository = Depends()):
-    return repository.get_all()
+# @router.get('/', response_model=List[ShowProductDiscountSchema])
+# def index(repository: ProductDiscountRepository = Depends()):
+#     return repository.get_all()
     
 
-@router.put(' /{id}')
-def update(id: int, productDiscount: ProductDiscountSchema,repository: ProductDiscountRepository = Depends()):
-   repository.update(id, productDiscount.dict())
+# @router.put(' /{id}')
+# def update(id: int, productDiscount: ProductDiscountSchema,repository: ProductDiscountRepository = Depends()):
+#    repository.update(id, productDiscount.dict())
 
-@router.get('/{id}', response_model = ShowProductDiscountSchema)
-def show(id: int, repository: ProductDiscountRepository = Depends()):
-    return repository.get_by_id(id)
+# @router.get('/{id}', response_model = ShowProductDiscountSchema)
+# def show(id: int, repository: ProductDiscountRepository = Depends()):
+#     return repository.get_by_id(id)
