90281d3aed1a277e0d2a29c97ee0d82c64304fdf
unknown <laura.moraes@faci.ly>
2021-12-13 15:33:38 -0300

    implementacao dos testes

---
+++ listaPython/src/media.py
@@ -0,0 +1,5 @@
+def calculo_media( nota1,nota2, nota3, nota4):
+    media = (nota1 + nota2 + nota3 + nota4)/4
+    return f'Nota 1: {nota1}, Nota 2: {nota2}, Nota 3: {nota3}, Nota 4: {nota4} e Média: {media}'
+
+
---
+++ listaPython/tests/test_media.py
@@ -0,0 +1,8 @@
+import pytest
+from src.media import calculo_media
+
+
+def test_media():
+    assert calculo_media(3,4,3,4)
+
+
